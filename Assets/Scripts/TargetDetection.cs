1c174b94beb4ec8454c1a8ac2350ca83e0b4e567
QwinteDeValck <qwinte.de.valck@student.ehb.be>
2025-10-10 12:54:59 +0200

    Basics of targetDetection on the player and objects

--- Assets/Scripts/TargetDetection.cs
+++ Assets/Scripts/TargetDetection.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class TargetDetection : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float detectionRange = 20f;
+    
     void Update()
     {
-        
+        DetectObjectInView();
+    }
+
+    void DetectObjectInView()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, detectionRange))
+        {
+            Debug.Log("Camera ziet object met tag: " + hit.collider.tag);
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * detectionRange, Color.red);
     }
 }
