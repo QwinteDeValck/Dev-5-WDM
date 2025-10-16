37e75aedaf338937251bf5e381c0919e3153e795
QwinteDeValck <qwinte.de.valck@student.ehb.be>
2025-10-16 17:24:49 +0200

    houd data bij in lijst + aantal kijktijd van het object

--- Assets/Scripts/TargetDetection.cs
+++ Assets/Scripts/TargetDetection.cs
@@ -1,24 +1,69 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetDetection : MonoBehaviour
 {
     [SerializeField] private float detectionRange = 20f;
-    
-    void Update()
+    [SerializeField] private float timeBeforeLog = 0.1f;
+    [System.Serializable]
+    public class LookedTarget
     {
-        DetectObjectInView();
+        public string targetTag;
+        public float lookDuration;
     }
 
-    void DetectObjectInView()
+    public List<LookedTarget> lookedTargets = new List<LookedTarget>();
+    private Transform currentTarget = null;
+    private float currentLookTime = 0f;
+
+    void Update()
+    {
+        DetectTarget();
+    }
+
+    void DetectTarget()
     {
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, detectionRange))
+        if (Physics.Raycast(ray, out hit, detectionRange, ~0, QueryTriggerInteraction.Collide))
         {
-            Debug.Log("Camera ziet object met tag: " + hit.collider.tag);
+            Transform hitTarget = hit.transform;
+
+            if (hitTarget == currentTarget)
+            {
+                currentLookTime += Time.deltaTime;
+            }
+            else
+            {
+                if (currentTarget != null && currentLookTime >= timeBeforeLog)
+                {
+                    SaveTargetData(currentTarget.tag, currentLookTime);
+                }
+
+                // Reset voor nieuw target
+                currentTarget = hitTarget;
+                currentLookTime = 0f;
+            }
+        }
+        else
+        {
+            if (currentTarget != null && currentLookTime >= timeBeforeLog)
+            {
+                SaveTargetData(currentTarget.tag, currentLookTime);
+            }
+
+            currentTarget = null;
+            currentLookTime = 0f;
         }
 
         Debug.DrawRay(transform.position, transform.forward * detectionRange, Color.red);
+    }
+
+    void SaveTargetData(string tag, float duration)
+    {
+        // Sla data op in lijst
+        lookedTargets.Add(new LookedTarget { targetTag = tag, lookDuration = duration });
+        Debug.Log($"Naar {tag} gekeken voor {duration:F2} seconden");
     }
 }
